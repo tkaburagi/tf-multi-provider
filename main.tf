0b1e4b4f1227cbe4afbbe66f6d41183072074e97
Takayuki Kaburagi <t.kaburagi@me.com>
2019-08-08 18:06:40 +0900

    first commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+    alias  = "account-1"
+    region = var.region
+    access_key = var.access_key
+    secret_key = var.secret_key
+}
+
+provider "aws" {
+    alias  = "account-2"
+    region = var.region
+    access_key = var.access_key_2
+    secret_key = var.secret_key_2
+}
+
+module "aws-1" {
+    source = "./modules/module-a"
+    providers = {
+        aws = aws.account-1
+    }
+}
+
+module "aws-2" {
+    source = "./modules/module-a"
+    providers = {
+        aws = aws.account-2
+    }
+}
---
+++ modules/module-a/networking.tf
@@ -0,0 +1,7 @@
+# VPC
+resource "aws_vpc" "playground" {
+    cidr_block = var.vpc_cidr
+    instance_tenancy = "default"
+    enable_dns_support = "true"
+    enable_dns_hostnames = "true"
+}
